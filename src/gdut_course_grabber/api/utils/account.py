7f64311f8554adb2886ffc05b27099d7ce8f2742
Ricky8955555 <rkmiao@duck.com>
2024-12-28 21:04:15 +0800

    docs: fix missing docs

--- src/gdut_course_grabber/api/utils/account.py
+++ src/gdut_course_grabber/api/utils/account.py
@@ -1,14 +1,27 @@
+"""
+提供帐户相关实用工具。
+"""
+
 from typing import Annotated
 
 from fastapi import Depends
 
 from gdut_course_grabber.models import Account
 
-
 __all__ = ["AccountDep"]
 
 
 def account_parameters(session_id: str) -> Account:
+    """
+    解析参数为帐户。
+
+    Args:
+        session_id (str): 会话 ID。
+
+    Returns:
+        Account: 解析结果。
+    """
+
     return Account(session_id=session_id)
 
 

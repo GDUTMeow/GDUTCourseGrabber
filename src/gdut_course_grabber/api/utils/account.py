d80b1927efce3e5653cb899f9c19ec8fa1c53a9e
Ricky8955555 <rkmiao@duck.com>
2024-12-28 21:20:10 +0800

    refactor(api): mark function protected

--- src/gdut_course_grabber/api/utils/account.py
+++ src/gdut_course_grabber/api/utils/account.py
@@ -11,7 +11,7 @@ from gdut_course_grabber.models import Account
 __all__ = ["AccountDep"]
 
 
-def account_parameters(session_id: str) -> Account:
+def _account_parameters(session_id: str) -> Account:
     """
     解析参数为帐户。
 
@@ -25,4 +25,4 @@ def account_parameters(session_id: str) -> Account:
     return Account(session_id=session_id)
 
 
-AccountDep = Annotated[Account, Depends(account_parameters)]
+AccountDep = Annotated[Account, Depends(_account_parameters)]

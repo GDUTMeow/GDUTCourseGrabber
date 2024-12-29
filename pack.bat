06b57b766b40aadf7835255753ced23f372dfa73
GamerNoTitle <GamerNoTitle@outlook.com>
2024-12-29 09:57:56 +0800

    Chore: packing script update

--- pack.bat
+++ pack.bat
@@ -2,4 +2,4 @@
 chcp 65001
 nuitka --standalone --include-data-dir=templates=templates --include-data-dir=static=static ^
 --windows-icon-from-ico=img/icon.ico --windows-company-name=GamerNoTitle --windows-product-name="GDUT抢课助手" ^
---windows-file-version=2.0.3 --windows-product-version=2.0.3 --lto=yes --assume-yes-for-downloads app.py
+--windows-file-version=2.0.4 --windows-product-version=2.0.4 --lto=yes --assume-yes-for-downloads app.py

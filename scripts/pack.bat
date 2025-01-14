26d852f8b099abd17df5a896947334e8c48fde8c
GamerNoTitle <GamerNoTitle@outlook.com>
2025-01-15 00:44:10 +0800

    refactor: remake the build script, moved icon's position and add more files to be ignored

--- scripts/pack.bat
+++ scripts/pack.bat
@@ -1,3 +1,8 @@
 @echo off
 chcp 65001
-nuitka --standalone --include-data-dir=templates=templates --include-data-dir=static=static --windows-icon-from-ico=img/icon.ico --windows-company-name=GamerNoTitle --windows-product-name="GDUT抢课助手" --windows-file-version=2.0 --windows-product-version=2.0 --lto=yes --assume-yes-for-downloads app.py
+pip install pdm
+pdm install
+pdm add --dev nuitka
+pdm run nuitka --standalone --include-data-dir=static=static --windows-icon-from-ico=static/icon.ico ^
+--windows-company-name=GamerNoTitle --windows-product-name="GDUT抢课助手" --windows-file-version=3.0 ^
+--windows-product-version=3.0 --lto=yes --assume-yes-for-downloads src/gdut_course_grabber/__main__.py

a292e90ab8930a6e48fe3c76180c3e1fd65f282b
Pesy Wu <GamerNoTitle@outlook.com>
2025-01-14 16:06:00 +0000

    fix: create data folder when not exists

--- src/gdut_course_grabber/__main__.py
+++ src/gdut_course_grabber/__main__.py
@@ -2,6 +2,7 @@
 GDUTCourseGrabber 程序入口。
 """
 
+import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -14,4 +15,6 @@ app.mount("/api", api.app)
 app.mount("/", StaticFiles(directory="static", html=True))
 
 if __name__ == "__main__":
+    if not os.path.exists("data"):
+        os.mkdir("data")
     uvicorn.run(app)

17d8ad477f31e170dea1c80f58aa8bc4550de058
Ricky8955555 <rkmiao@duck.com>
2025-06-26 01:22:07 +0800

    feat: random port

--- src/gdut_course_grabber/__main__.py
+++ src/gdut_course_grabber/__main__.py
@@ -3,6 +3,7 @@ GDUTCourseGrabber 程序入口。
 """
 
 import webbrowser
+import socket
 
 import uvicorn
 from fastapi import FastAPI
@@ -11,11 +12,18 @@ from fastapi.staticfiles import StaticFiles
 from gdut_course_grabber import api
 from gdut_course_grabber.constants import STATIC_PATH
 
+
 app = FastAPI()
 
 app.mount("/api", api.app)
 app.mount("/", StaticFiles(directory=STATIC_PATH, html=True))
 
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:8000")
-    uvicorn.run(app)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind(("127.0.0.1", 0))
+        sock.listen()
+
+        port = sock.getsockname()[1]
+        webbrowser.open(f"http://localhost:{port}")
+
+        uvicorn.run(app, fd=sock.fileno())

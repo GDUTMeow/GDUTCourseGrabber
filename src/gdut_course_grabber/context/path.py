c81e6bed8b71b8ded150832483cda9d6a35297f6
Ricky8955555 <rkmiao@duck.com>
2025-06-26 11:36:33 +0800

    docs: add missing docs

--- src/gdut_course_grabber/context/path.py
+++ src/gdut_course_grabber/context/path.py
@@ -1,3 +1,7 @@
+"""
+提供路径上下文。
+"""
+
 from pathlib import Path
 
 import platformdirs
@@ -5,7 +9,13 @@ import platformdirs
 from gdut_course_grabber.utils.path import search_path
 
 static_path = search_path(Path(__file__).parent, "static", max_depth=2)
+"""
+静态资源路径。
+"""
 
 platform_dirs = platformdirs.PlatformDirs(
     appname="GDUTCourseGrabber", appauthor="GDUTMeow", version="v3", ensure_exists=True
 )
+"""
+平台相关文件夹。
+"""

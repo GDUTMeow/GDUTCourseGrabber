71ae1e8387169adab96541a656a34676efc1137a
Ricky8955555 <rkmiao@duck.com>
2025-06-26 12:28:37 +0800

    fix(path): unable to search static path

--- src/gdut_course_grabber/context/path.py
+++ src/gdut_course_grabber/context/path.py
@@ -8,7 +8,7 @@ import platformdirs
 
 from gdut_course_grabber.utils.path import search_path
 
-static_path = search_path(Path(__file__).parent, "static", max_depth=2)
+static_path = search_path(Path(__file__).parent.parent, "static", max_depth=2)
 """
 静态资源路径。
 """

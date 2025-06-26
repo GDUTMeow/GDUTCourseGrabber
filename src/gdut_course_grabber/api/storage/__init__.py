e2ddc0bd7fcfcb6a143c505423a343885dcfe382
Ricky8955555 <rkmiao@duck.com>
2025-06-26 12:44:11 +0800

    feat: storage

---
+++ src/gdut_course_grabber/api/storage/__init__.py
@@ -0,0 +1,47 @@
+"""
+提供存储相关 API。
+"""
+
+from fastapi import APIRouter, Request
+
+from gdut_course_grabber.api.exc import entity_not_found_error
+from gdut_course_grabber.api.types import ApiResponse
+from gdut_course_grabber.context.storage import storage_manager
+
+router = APIRouter()
+
+
+@router.get("/{key}")
+def get(key: str) -> ApiResponse[bytes]:
+    """
+    获取指定键相应存储数据。
+
+    Args:
+        key (str): 所需获取存储数据的键。
+    """
+
+    try:
+        value = storage_manager.get(key)
+    except KeyError as ex:
+        raise entity_not_found_error(key) from ex
+
+    return ApiResponse(data=value)
+
+
+@router.post("/{key}")
+async def set(request: Request, key: str) -> ApiResponse[None]:
+    """
+    向指定键存储数据。
+
+    Args:
+        key (str): 用于存储数据的键。
+    """
+
+    body = await request.body()
+
+    try:
+        storage_manager.set(key, body)
+    except KeyError as ex:
+        raise entity_not_found_error(id) from ex
+
+    return ApiResponse(data=None)
